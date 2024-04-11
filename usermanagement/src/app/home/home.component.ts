230aa56ab5c55d3d888a2b9270ef4c1e1fe68853
KunalSax10 <kunal.saxena219@gmail.com>
2024-04-11 21:26:12 +0530

    Your commit message here

--- usermanagement/src/app/home/home.component.ts
+++ usermanagement/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  userForm!: FormGroup;
+  constructor(private formbuilder: FormBuilder) { }
   ngOnInit(): void {
+    this.userForm = this.formbuilder.group({
+      fullName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      role: [null, [Validators.required]],
+      address:[null],
+      mobileNo: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    })
   }
 
+  @ViewChild('myModal') myModal!: ElementRef;
+  openModal() {
+    const modal = this.myModal.nativeElement;
+    modal.classList.add('show');
+    modal.style.display = 'block';
+    modal.setAttribute('aria-modal', 'true');
+  }
+
+  closeModal() {
+    const modal = this.myModal.nativeElement;
+    modal.classList.remove('show');
+    modal.style.display = 'none';
+    modal.removeAttribute('aria-modal');
+  }
+
+  resetForm() {
+    this.userForm.reset();
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
 }
--- usermanagement/src/app/login/login.component.ts
+++ usermanagement/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -19,4 +19,26 @@ export class LoginComponent implements OnInit {
     })
   }
 
+  @ViewChild('myModal') myModal!: ElementRef;
+  openModal() {
+    const modal = this.myModal.nativeElement;
+    modal.classList.add('show');
+    modal.style.display = 'block';
+    modal.setAttribute('aria-modal', 'true');
+  }
+
+  closeModal() {
+    const modal = this.myModal.nativeElement;
+    modal.classList.remove('show');
+    modal.style.display = 'none';
+    modal.removeAttribute('aria-modal');
+  }
+
+  SubmitLoginForm(){
+    if(this.LoginForm.valid){
+
+    }else{
+      this.LoginForm.markAllAsTouched
+    }
+  }
 }
