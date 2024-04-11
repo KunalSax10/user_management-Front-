b34a0eabc9bdb0c886e4795f0f94824d7cf98fa7
yogeshumale <umale.dnk@gmail.com>
2024-04-11 17:04:21 +0530

    commit

--- usermanagement/src/app/app.module.ts
+++ usermanagement/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,6 +7,7 @@ import { CreateuserComponent } from './createuser/createuser.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     LoginComponent,
     CreateuserComponent,
     HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
