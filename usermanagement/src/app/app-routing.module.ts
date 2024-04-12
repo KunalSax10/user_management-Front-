b9800799ebbdac7b47b4937e1fab5c9d5ae6adef
yogeshumale <umale.dnk@gmail.com>
2024-04-12 09:04:35 +0530

    routing issue

--- usermanagement/src/app/app-routing.module.ts
+++ usermanagement/src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { CreateuserComponent } from './createuser/createuser.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'create', component: CreateuserComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
