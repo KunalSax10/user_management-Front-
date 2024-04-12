25932e16834deb0ac62ee92b3b603c6654012b64
yogeshumale <umale.dnk@gmail.com>
2024-04-12 09:02:27 +0530

    Remove extra component

--- usermanagement/src/app/app.module.ts
+++ usermanagement/src/app/app.module.ts
@@ -3,19 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { CreateuserComponent } from './createuser/createuser.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MenuComponent } from './menu/menu.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CreateuserComponent,
     HomeComponent,
-    MenuComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +20,7 @@ import { MenuComponent } from './menu/menu.component';
     SharedModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
